cd2d3bd6656156fbaf3265a14fca7f1391261159
Gabriel_Chan <gabriel.chanyk@gmail.com>
2020-09-22 17:08:31 -0400

    Initial Commit

---
+++ BankApp.c
@@ -0,0 +1,16 @@
+/*
+Program:	Bank Account Management Application
+Author:		Gabriel, Chan
+Date:		July 20, 2018
+Purpose:	Keep track of account balance allowing user to deposit,withdraw,accumulate
+interest, and show the balance.
+*/
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	return 0;
+}
